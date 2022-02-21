a8b78d0f75583d179999e918ec7bb5b24e31ad8e
Liudmila111983 <lamurashevich@gmail.com>
2022-02-21 21:10:41 +0300

    newJS

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const removeTransition = (e) => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("transform");
+};
 const playSound = (event) => {
-  console.log(event);
+  // console.log(event);
   const button = document.querySelector(`div[data-key='${event.keyCode}']`);
-  const audio = document.querySelector(`audio[data-key='${event.keyCode}]`);
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+
   if (!audio) return; //чтобы не было ошибки если аудио еще не загрузилось, а кнопка уже нажата
-  button.classList.add("play"); // добавляем класс к диву в html
+  button.classList.add("transform"); // добавляем класс к диву в html
   audio.currentTime = 0; // обнуляем время проигрывания, чтобы при повторном нажатии музыка воспроизвелась опять
   audio.play();
-  console.log("OK");
 };
 
+const keys = document.querySelectorAll(".key");
+console.log(keys);
+keys.forEach((el) => el.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound); // в функцию ничего не передаем
